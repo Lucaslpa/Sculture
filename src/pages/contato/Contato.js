f31e1b0f23753c55b4d919ce8927d17cf7b4196e
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-06-07 23:31:34 -0300

    add Contato component

--- src/pages/contato/Contato.js
+++ src/pages/contato/Contato.js
@@ -1,32 +1,45 @@
-
+import {useEffect} from 'react'
 
 
 
 export const Contato =  () => {
+
+
+
+    useEffect(() => {
+        const catalogo = document.getElementById('Catálogo-link')
+        const media = document.getElementById('Footer-Media')
+        media.style.marginTop = '70px'
+        catalogo.style.display = 'none'
+ 
+     }, [])
     return ( 
         <main id='Contato' >
-            <div >
+            <div id='Contato-texts'>
                    <h4>Fale conosco</h4>
                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             </div>
-             <form>
-                <fieldset>
+             <form id='Contato-Form' >
+                <div id='Contato-form-name'>
                     <legend>Nome</legend>
-                    <input placeholder='Nome' type='email'/>
+                   <div>
+                   <input placeholder='Nome' type='email'/>
                     <input placeholder='Sobrenome' type='email'/>
-                </fieldset>
-                <fieldset>
+                   </div>
+                </div>
+                <fieldset id='Contato-form-email' >
                     <legend>Email</legend>
-                    <input type='emaiol' ></input>
+                    <input type='email' ></input>
                 </fieldset>
-                <fieldset>
+                <fieldset id='Contato-form-subject' >
                     <legend>Assunto</legend>
                     <input type='text'></input>
                 </fieldset>
-                <fieldset>
+                <fieldset id='Contato-form-message' >
                     <legend>Mensagem</legend>
-                    <input type='text' ></input>
+                    <textarea type='text' ></textarea>
                 </fieldset>
+                <button>Enviar</button>
              </form>
         </main>  
     )
