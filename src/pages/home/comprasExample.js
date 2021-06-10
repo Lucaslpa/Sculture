e91d8e740d9dc69e5f98efb920d6414443c9f1d1
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-06-09 21:36:31 -0300

    new items to store

--- src/pages/home/comprasExample.js
+++ src/pages/home/comprasExample.js
@@ -1,49 +1,58 @@
 
 export const clothesArray = [
-    {  id: 1, 
-       title:'Goku roupa super realista' ,
-       link: 'https://canary.contestimg.wish.com/api/webimage/5c1c835b2137682bb1023a4c-large.jpg?cache_buster=f5d68de20157d01806b82f3eb90d924f',
-       price: 55.99
-    },
-    {  
-       id: 2,  
-      title:'Luffy roupa super realista' ,
-    link: 'https://images-na.ssl-images-amazon.com/images/I/414igfAFIbL._AC_.jpg',
-    price: 50.99,
- },
-  {  id: 3, 
-    title:'Goku roupa super realista' ,
-       link: 'https://canary.contestimg.wish.com/api/webimage/5c1c835b2137682bb1023a4c-large.jpg?cache_buster=f5d68de20157d01806b82f3eb90d924f',
-       price: 55.99
-    },
-    {  id: 4,
-       title:'Luffy roupa super realista' ,
-    link: 'https://images-na.ssl-images-amazon.com/images/I/414igfAFIbL._AC_.jpg',
-    price: 50.99,
- },{  
-    id: 5,  
-   title:'Goku roupa super realista' ,
- link: 'https://canary.contestimg.wish.com/api/webimage/5c1c835b2137682bb1023a4c-large.jpg?cache_buster=f5d68de20157d01806b82f3eb90d924f',
- price: 55.99,
- promotion: 30.99
+   {  id: 1, 
+      title:'Titâ Eren yeager' ,
+      link: 'https://i.pinimg.com/originals/fc/44/56/fc445664f5f62a87c6bc22736a8f70e5.jpg',
+      price: 200,
+      color: false, 
+   },
+   {  
+     id: 8, 
+     title:'Monkey D. Luffy' ,
+     link: 'https://www.hlj.com/media/catalog/product/cache/acedba8d3f43cedb2fbb4f1aa3b47451/u/a/uas29001_0.jpg',
+     price: 320,
+     promotion: 290, 
+     color: true 
+     },
+   {  
+      id: 2,  
+     title:'Vegeta' ,
+   link: 'https://i.pinimg.com/originals/be/2f/1a/be2f1a25709f0dabb2e48a62e8919a12.jpg',
+   price: 230,
+   color: false
+},
+ {  id: 3, 
+   title:'Gaara' ,
+      link: 'https://i.pinimg.com/originals/e2/09/19/e20919df9f8573eb8365ae1753793d6f.jpg',
+      price: 250,
+      color: false,
+   },
+   {  id: 4,
+      title:'Goku' ,
+   link: 'https://i.pinimg.com/originals/da/42/bb/da42bb0e427638046b50ed727052096e.jpg',
+   price: 300,
+   color: true
+},{  
+   id: 5,  
+  title:'Barba Branca' ,
+link: 'https://ae01.alicdn.com/kf/HTB1hyGIKVXXXXapXFXXq6xXFXXXB/Mostrar-Z-Store-40-cm-Modelo-GK-ONE-PIECE-Barba-Branca-Edward-Newgate-Figura-Est.jpg_Q90.jpg_.webp',
+price: 500,
+promotion: 430,
+color: false 
 },
 { id: 6, 
-title:'Luffy roupa super realista' ,
-link: 'https://images-na.ssl-images-amazon.com/images/I/414igfAFIbL._AC_.jpg',
-price: 50.99,
-promotion: 45.99
+title:'Izuku Midoriya' ,
+link: 'https://i.pinimg.com/originals/c3/ff/81/c3ff8100908be166217c5303d42b5403.png',
+price: 350,
+promotion: 320,
+color: true
 },{id: 7,   
-   title:'Goku roupa super realista' ,
-link: 'https://canary.contestimg.wish.com/api/webimage/5c1c835b2137682bb1023a4c-large.jpg?cache_buster=f5d68de20157d01806b82f3eb90d924f',
-price: 55.99
+  title:'Link' ,
+link: 'https://static3.tcdn.com.br/img/img_prod/460977/pre_venda_estatua_link_legend_of_zelda_breath_of_the_wild_first_4_figures_19607_1_20201211173259.jpg',
+price: 300,
+color: true
 },
-{  
-id: 8, 
-title:'Luffy roupa super realista' ,
-link: 'https://images-na.ssl-images-amazon.com/images/I/414igfAFIbL._AC_.jpg',
-price: 50.99,
-promotion: 45.99
-}
+
 ]
 
 
