7b67b1a9d2e34c5367ec194d5b73a3d55b187a77
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-06-13 21:31:40 -0300

    mod

--- src/pages/home/function.js
+++ src/pages/home/function.js
@@ -2,7 +2,7 @@ import {useEffect, useState, useContext,  createContext} from 'react'
 
 export function getTotalItemsInArrayFromLocalStorage(setState) {
     const cartItemsArray = JSON.parse(localStorage.getItem('clothes'))
-    if(!cartItemsArray[0]) {
+    if(!cartItemsArray) {
            return 
     }
     setState(cartItemsArray.length)
